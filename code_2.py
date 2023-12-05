1b3ae1eeae987b20cf8b3970894ee1f89c4657ca
semenk69 <152292047+semenk69@users.noreply.github.com>
2023-12-05 09:12:35 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+    public static void main(String[] args) {
+    }
+
+        }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+}
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;

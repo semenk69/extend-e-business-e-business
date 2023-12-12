05af5fc7945bb79f6f7be73f5ee6953f45a79105
semenk69 <152292047+semenk69@users.noreply.github.com>
2023-12-13 01:21:33 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,38 +1,51 @@
-
-}
-            data[i] = (int) (Math.random() * 100) + 1;
-    public static void main(String[] args) {
-
-        }
-        for (int i = 0; i < 10; i++) {
-public class RandomDataGenerator {
-        for (int item : data) {
         int[] data = new int[10];
-        }
-    }
-            System.out.println("Random Number: " + item);}
-        }
-
-    }
-    public static void main(String[] args) {
-            data[i] = (int) (Math.random() * 100) + 1;
-
 public class RandomDataGenerator {
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        }
         }
             System.out.println("Random Number: " + item);
-        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+
         for (int item : data) {
-        int[] data = new int[10];    data = [random.randint(1, 100) for _ in range(10)]
+    }
+}
+def generate_random_data():
+
 
     data = generate_random_data()
-
-        print(f"Random Number: {item}")
 import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+    for item in data:
+if __name__ == "__main__":
 
 def main():
+        print(f"Random Number: {item}")
+    main()
+    data = generate_random_data()
+def generate_random_data():
+
+import random
+
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    for item in data:
+if __name__ == "__main__":
 
     main()
-if __name__ == "__main__":
     return data
-def generate_random_data():
-    for item in data:
+}
+    }
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+
+        }
+    public static void main(String[] args) {
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
